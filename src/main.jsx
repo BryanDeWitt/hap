bdfc18b97552cb018626b56b3dc28f23921a4308
Bryan <tolovehayley@gmail.com>
2025-03-29 18:58:05 -0300

    a ver ahora

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { JavaScript } from './JavaScript.jsx'
 import { SolucionesJS } from './SolucionesJS.jsx'
 import { Solucion } from './Solucion.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/soluciones/javaScript',
-    element: <JavaScript />,
-  },
-  {
-    path: '/soluciones/javaScript/:id',
-    element: <SolucionesJS />,
-  },
-  {
-    path: '/soluciones/javaScript/:id/:index',
-    element: <Solucion />,
-  }
-])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <RouterProvider router={router} />
-    </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/soluciones/javaScript" element={<JavaScript />} />
+        <Route path="/soluciones/javaScript/:id" element={<SolucionesJS />} />
+        <Route path="/soluciones/javaScript/:id/:index" element={<Solucion />} />
+      </Routes>
+    </HashRouter>
   </StrictMode>,
 )
