1a4caa296c6bc9c10106b4025b16b9e6f6303d42
Bryan <tolovehayley@gmail.com>
2025-03-29 18:55:53 -0300

    a ver ahora

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { JavaScript } from './JavaScript.jsx'
 import { SolucionesJS } from './SolucionesJS.jsx'
 import { Solucion } from './Solucion.jsx'
@@ -28,6 +28,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <RouterProvider router={router} />
+    </BrowserRouter>
   </StrictMode>,
 )
