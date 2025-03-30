f2031259ddea897cd217bab018f5e9ba1984bbfd
Bryan <tolovehayley@gmail.com>
2025-03-30 15:37:52 -0300

    agregados botones para ir a la siguiente solucion o la anterior

--- src/Solucion.jsx
+++ src/Solucion.jsx
@@ -7,16 +7,22 @@ import { useState } from 'react'
 export const Solucion = () => {
     const [show, setShow] = useState(false)
     const { index, id } = useParams()
+    const maxIndex = leccionesArray[id].length - 1
     const leccion = leccionesArray[id][index]
     const solucion = soluciones[leccion]
     const handleShow = () => {
         setShow(!show)
     }
+    
     return (
         <>
             <h1>Solucion para: { leccion }</h1>
             <h3 className='explicacion'>{ solucion.explicacion ? solucion.explicacion : 'No se ha encontrado la solucion' }</h3>
-            <button onClick={handleShow}>{ show ? 'Ocultar codigo' : 'Mostrar codigo' }</button>
+            <div className='botones'>
+                <Link to={`/soluciones/javaScript/${id}/${Number(index) - 1 < 0 ? 0 : Number(index) - 1}`}><button>Anterior</button></Link>
+                <button onClick={handleShow}>{ show ? 'Ocultar codigo' : 'Mostrar codigo' }</button>
+                <Link to={`/soluciones/javaScript/${id}/${Number(index) + 1 > maxIndex ? maxIndex : Number(index) + 1}`}><button>Siguiente</button></Link>
+            </div>
             { solucion.pseudocode && 
                 <>
                 <h2>Pseudo código</h2>
