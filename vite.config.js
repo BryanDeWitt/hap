e0c3c0bee6cbed58d12703fae3c86cdbf6bbf629
BryanLuzardo <106171478+BryanDeWitt@users.noreply.github.com>
2025-04-01 01:17:38 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ayudajap2025.lat"
+  base: "https://ayudajap2025.lat/hap"
 })
